a609c9caca3ab1e1ebb8e00c430b94348441cffb
root <624191343@qq.com>
2018-04-13 17:36:48 +0800

    集成sso单点登录

---
+++ open-oauth-center/open-oauth-sso/src/main/java/com/open/capacity/SsoClientApp.java
@@ -0,0 +1,37 @@
+package com.open.capacity;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 作者 owen E-mail: wang.wen@neusoft.com
+* @version 创建时间：2017年11月12日 上午22:57:51
+*/
+
+@RestController
+@SpringBootApplication
+public class SsoClientApp {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	@RequestMapping(value = { "/user" }, produces = "application/json") // 获取用户信息。/auth/user
+    public Principal user(Principal user) {
+        return user;
+    }
+	@GetMapping("/users")
+	public Authentication user(Authentication user) {
+		return user;
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SsoClientApp.class, args);
+	}
+}
---
+++ open-oauth-center/open-oauth-sso/src/main/java/com/open/capacity/client/web/UnieapSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.open.capacity.client.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.stereotype.Component;
+
+/** 
+* @author 作者 owen E-mail: wang.wen@neusoft.com
+* @version 创建时间：2017年11月12日 上午22:57:51 
+* 类说明 
+*  @EnableOAuth2Sso注解。如果WebSecurityConfigurerAdapter类上注释了@EnableOAuth2Sso注解，
+*  那么将会添加身份验证过滤器和身份验证入口。
+*  如果只有一个@EnableOAuth2Sso注解没有编写在WebSecurityConfigurerAdapter上，
+*  那么它将会为所有路径启用安全，并且会在基于HTTP Basic认证的安全链之前被添加。详见@EnableOAuth2Sso的注释。
+* 
+*/
+@Component
+@Configuration
+@EnableOAuth2Sso
+public   class UnieapSecurityConfig extends WebSecurityConfigurerAdapter{
+	 
+	   @Override
+       public void configure(HttpSecurity http) throws Exception {
+           http.formLogin().and()
+                   .antMatcher("/**")
+                   // 所有请求都得经过认证和授权
+                   .authorizeRequests().anyRequest().authenticated()
+                   .and().authorizeRequests().antMatchers("/","/anon").permitAll()
+                   .and()
+                   // 这里之所以要禁用csrf，是为了方便。
+                   // 否则，退出链接必须要发送一个post请求，请求还得带csrf token
+                   // 那样我还得写一个界面，发送post请求
+                   .csrf().disable()
+                   // 退出的URL是/logout
+                   .logout().logoutUrl("/logout").permitAll()
+                   // 退出成功后，跳转到/路径。
+                   .logoutSuccessUrl("/login");
+       }
+}
+	
+
+	
