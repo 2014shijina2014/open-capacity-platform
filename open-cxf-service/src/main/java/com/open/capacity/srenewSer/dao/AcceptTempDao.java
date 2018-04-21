12f6c0041ad08df1dbdef0f00904f9f16e49a14d
root <624191343@qq.com>
2018-04-21 21:54:15 +0800

    cxf集成

--- open-cxf-service/src/main/java/com/open/capacity/srenewSer/dao/AcceptTempDao.java
+++ open-cxf-service/src/main/java/com/open/capacity/srenewSer/dao/AcceptTempDao.java
@@ -43,17 +43,67 @@ public interface AcceptTempDao {
 			+ " APPLY_START_DATE,SERVICE_START_DATE,  DEALER_RETURN_DATE, DEALER_INPUT_DATE,STATUS_CHANGE_DATE,"
 			+ "BRAND_ORG,SHARE_SCALE, NET_GRID)" + " VALUES ("
 			+ "#{OS_TEMP_RGST}, 0, #{CITY_CODE}, 302,  TO_DATE (#{accept_date}, 'YYYYMMDDHH24MISS'),"
-			+ " #{oper_id},'4750684', 1, 0, #{CITY_CODE},"
+			+ " #{oper_id},#{channel_id}, 1, 0, #{CITY_CODE},"
 			+ " 0,   '75a17x',  '075750047',  #{USER_ID}, #{CUSTOMER_ID},"
 			+ " #{ACCOUNT_ID}, 0, #{INHERIT_ID}, #{service_id},  #{service_kind}," + "#{INNET_METHOD}, -1, 0, 0, 0,"
 			+ " #{PASSWORD}, 1, #{PAY_TYPE},#{DEVELOPER_DEALER},#{DEVELOPER},"
 			+ " #{SERVICE_FAVOUR_ID}, 0, 0,   0,  3,"
 			+ " 0,0,  0,  0,  0," + " 0, 0,  0,  1, '2015-09-08'," + " 1, #{REAL_INSTALL_ADDRESS}, 0,  '0',  0,"
-			+ " 0,0,  0, '2',   0," + " 0,  0,   0,  0, 0," + " 0,  0,  0,   0,  0," + " '4750684',   0,  0, 0, 0,"
+			+ " 0,0,  0, '2',   0," + " 0,  0,   0,  0, 0," + " 0,  0,  0,   0,  0," + " #{channel_id},   0,  0, 0, 0,"
 			+ "  0,0, '0', 0, 0," + " 1,  0,  '0',  0,   0,"
 			+ " #{APPLY_START_DATE},  #{SERVICE_START_DATE}, #{DEALER_RETURN_DATE} , #{DEALER_INPUT_DATE} ,  #{STATUS_CHANGE_DATE} ,"
 			+ " 0, 0,   #{NET_GRID})")
 	public int saveWideTemp(Map<String, Object> map);
+	
+	
+	
+	@Insert("INSERT INTO CRM_OWNER_USER.BB_ADSL_BUS_TEMP_T ("
+			+ "REGISTER_NUMBER, ACCEPT_STATUS, ACCEPT_CITY,   APPLY_EVENT,  ACCEPT_DATE,"
+			+ "ACCEPT_PERSON,  ACCEPT_ADDRESS_ID,ACCEPT_WAY,APPLY_REASON,  CITY_CODE, "
+			+ "BUNDLE_ID,REGION_CODE, BELONG_CODE, USER_ID, CUSTOMER_ID,  "
+			+ "ACCOUNT_ID, INHERIT_FLAG,INHERIT_ID, SERVICE_ID, OPER_LEVEL, "
+			+ "INNET_METHOD,DEALER_INNET_METHOD,  CREDIT_SOLUTION,CREDIT_SOLU, IF_SELF_CONTROL, "
+			+ "PASSWORD, SERV_CREDIT_CONTROL,PAY_TYPE,  DEVELOPER_DEALER,   DEVELOPER,  "
+			+ "SERVICE_FAVOUR_ID,SERVICE_GROUP_ID, SERVICE_IF_VALID,SER_SERVING_STATUS,   ACCESS_LEVEL,"
+			+ "BUS_FAVOUR_ID, PAY_WAY, INTELLIGENTIZE_FLAG,  MONTH_FEE, IP_NUM,"
+			+ "ACCESS_MAX_NUM,  IDLE_TIMEOUT,  SESSION_TIMEOUT,  EXPIRATION_FLAG,  BANDWIDTH, "
+			+ "TRUNKS_COUNT,INSTALL_KIND,  IF_SPAN_BUREAU,   INSTALL_ADDRESS, LINE_NO,   "
+			+ "OLD_LINE_NO,  DATA_USER_LEVEL,  AVAILABLE,    IF_BIND_ACCOUNT, IF_BIND,  "
+			+ "CONTRACT_TIME, RATE, NET_CIRCUIT, IF_INSTALL,   SCHOOL_ID,"
+			+ "POS_FEE,MONEY_FEE, MONEY_WAY, MONTH_NUM,  PC_BANK,"
+			+ "RADIUS_LEVEL, PROJECT_ID, DEVICE_ID,IF_BIND_CARD, ANNUAL_NUMBER,"
+			+ "ANNUAL_FEE, PRESENT_MONTH,  PRESENT_FEE1, IF_CONTINUE,  PRE_ANNUAL_NUMBER,"
+			+ "PRE_ANNUAL_FEE, PRE_PRESENT_MONTH, PRE_PRESENT_FEE1,  PRE_CONTINUE_FLAG, ANNUAL_FLAG, "
+			+ "CONTRACT_MONTHS,   PRESENT_FEE, SOURCE_ID,  IF_PRE_COOP,  IF_EMERGENCY, "
+			+ "IF_PRE_FEE, IF_DEPOSIT, IF_RESERVE,   ACCEPT_SOURCE, BRAND_ID,  "
+			+ "VIA_ID_KIND, GRT_ID,GRT_IDENTITY_KIND, GRT_DEPOSIT, GROUP_ID,  "
+			+ "USER_TYPE, SALES_MODE, APPLY_START_DATE,  SERVICE_START_DATE, SERVICE_STOP_DATE, "
+			+ "DEALER_RETURN_DATE, DEALER_INPUT_DATE,  STATUS_CHANGE_DATE, NEW_ACC_TYPE,  IN_TYPE,"
+			+ "PRODUCT_WAY, BIND_TYPE, BRAND_ORG, SHARE_SCALE,  NET_GRID)"
+			+ "VALUES (#{OS_TEMP_RGST},1, #{CITY_CODE},   302,    TO_DATE (#{accept_date}, 'YYYYMMDDHH24MISS'),"
+			+ "#{oper_id},    #{channel_id},   1,  0, #{CITY_CODE},"
+			+ "0,    '075740006',  '075740006',   #{USER_ID},  #{CUSTOMER_ID},"
+			+ "#{ACCOUNT_ID},  0, #{INHERIT_ID},  #{service_id},  #{service_kind},"
+			+ "1, -1,  0,   0,   0,"
+			+ "#{PASSWORD},  1,  4,  '75b1lvb',  '7504683477',"
+			+ " '51018',   0,  0,   0,   3,"
+			+ " 0,   0,  0,   0, 0,"
+			+ " 0,  0,   0,   1,  '4',"
+			+ "  0,   2, 0,  #{INSTALL_ADDRESS},  0,"
+			+ " 0, 1, 0, 0,0,"
+			+ " 0,'4M', 0,   0,  0,"
+			+ " 0,  0,    0,0,  0,"
+			+ "  0,  0, 0, 0,   0,"
+			+ "  0, 0, 0,0, 0,"
+			+ " 0, 0, 0,0,  0,"
+			+ " 0,  0,  'C74012725',   0, 0,"
+			+ " 0, 0,  0,  0,'0',"
+			+ " 0, 0,  1,  0,   '0',"
+			+ " 0,  0,  #{APPLY_START_DATE}, #{SERVICE_START_DATE},#{SERVICE_STOP_DATE},"
+			+ " #{DEALER_RETURN_DATE},#{DEALER_INPUT_DATE},  #{STATUS_CHANGE_DATE},  0,  0,"
+			+ " 0,  0,   0,  0, #{NET_GRID})")
+	public int saveAdslTemp(Map<String, Object> map);
+	
 
 	@Insert("Insert into CRM_OWNER_USER.BB_BULK_SALE_BUS_TEMP_T(REGISTER_NUMBER, CITY_CODE, SERVICE_KIND, SERVICE_ID, USER_ID, "
 			+ "BULK_PRICE, FAVOUR_GROUP, SALES_MODE, PERIOD_TYPE, EFFECT_VALUES, " + "BEGIN_DATE, END_DATE) "
@@ -67,18 +117,26 @@ public interface AcceptTempDao {
 	@Select("select * from bb_service_relation_t where service_kind = #{service_kind} and service_id=#{service_id} and city_code = #{eparchy_code} and if_valid=1 ")
 	public Map getServiceInfo(Map<String, Object> map);
 
-	@Select("select * from bb_bulk_sale_info_t where service_id = #{service_id}  and service_kind =#{service_kind}  ")
+	@Select("select * from bb_bulk_sale_info_t where service_id = #{service_id}  and service_kind =#{service_kind} and   end_date> sysdate ")
 	public Map getBulkInfo(Map<String, Object> map);
 
-	@Select("select count(1) cnt from bb_bus_control_t  where service_kind = #{service_kind} and service_id=#{service_id} and accept_city = #{eparchy_code} ")
+	@Select("select * from bb_bus_control_t  where service_kind = #{service_kind} and service_id=#{service_id} and accept_city = #{eparchy_code} and rownum<=1")
 	public Map getFlowInfo(Map<String, Object> map);
 
 	@Select("   select * from bb_wide_service_info_t where user_id= #{user_id}")
 	public Map getWidefo(Map<String, Object> map);
 
+	
+	@Select("   select * from bb_adsl_service_info_t where user_id= #{user_id}")
+	public Map getAdslfo(Map<String, Object> map);
+	
 	@Select(" select * from BB_BULK_SALE_BUS_TEMP_T where register_number =  #{register_number}")
 	public Map getServiceKindByReg(Map<String, Object> map);
 
 	public void submit(Map<String, Object> map);
+	
+	
+	@Select("select * from pm_product_T where  f_prod_id=  #{SERVICE_FAVOUR_ID}")
+	public Map getProductInfo(Map<String, Object> map);
 
 }
--- open-cxf-service/src/main/java/com/open/capacity/srenewSer/utils/SrenewSerUtils.java
+++ open-cxf-service/src/main/java/com/open/capacity/srenewSer/utils/SrenewSerUtils.java
@@ -2,11 +2,18 @@ package com.open.capacity.srenewSer.utils;
 
 import java.util.logging.Logger;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.open.capacity.srenewSer.bo.SrenewSerBO;
+import com.open.capacity.srenewSer.bo.impl.SrenewSerBOImpl;
 import com.open.capacity.utils.SpringUtil;
 
-import cn.chinaunicom.ws.srenewser.SrenewSerImpl;
+import cn.chinaunicom.ws.srenewser.unibssbody.QRYCHGPRODOUTPUT;
+import cn.chinaunicom.ws.srenewser.unibssbody.SRENEWSUBOUTPUT;
 import cn.chinaunicom.ws.srenewser.unibssbody.SRENEWTRADEOUTPUT;
+import cn.chinaunicom.ws.srenewser.unibssbody.qrychgprodreq.QRYCHGPRODREQ;
+import cn.chinaunicom.ws.srenewser.unibssbody.qrychgprodrsp.QRYCHGPRODRSP;
+import cn.chinaunicom.ws.srenewser.unibssbody.srenewsubrsp.SRENEWSUBRSP;
 import cn.chinaunicom.ws.srenewser.unibssbody.srenewtradersp.SRENEWTRADERSP;
 
 /**
@@ -16,7 +23,11 @@ import cn.chinaunicom.ws.srenewser.unibssbody.srenewtradersp.SRENEWTRADERSP;
 public class SrenewSerUtils {
 
 	public static SrenewSerUtils instance;
+	
+	private ObjectMapper objectMapper =  SpringUtil.getBean(ObjectMapper.class) ;
 
+	private static final Logger LOG = Logger.getLogger(SrenewSerBOImpl.class.getName());
+	
 	private SrenewSerUtils() {
 
 	}
@@ -29,7 +40,6 @@ public class SrenewSerUtils {
 		return instance;
 	}
 
-	private static final Logger LOG = Logger.getLogger(SrenewSerUtils.class.getName());
 
 	/*
 	 * (non-Javadoc)
@@ -39,14 +49,43 @@ public class SrenewSerUtils {
 	 */
 	public cn.chinaunicom.ws.srenewser.unibssbody.QRYCHGPRODOUTPUT qryChgProd(
 			cn.chinaunicom.ws.srenewser.unibssbody.QRYCHGPRODINPUT parameters) {
-		LOG.info("Executing operation qryChgProd");
-		LOG.info(parameters.toString());
+		
 		try {
 			cn.chinaunicom.ws.srenewser.unibssbody.QRYCHGPRODOUTPUT _return = SpringUtil.getBean(SrenewSerBO.class).qryChgProd(parameters);
+			
+			try {
+				LOG.info("响应的报文"+objectMapper.writeValueAsString(_return));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			return _return;
 		} catch (Exception ex) {
-			ex.printStackTrace();
-			throw new RuntimeException(ex);
+			QRYCHGPRODOUTPUT out = new QRYCHGPRODOUTPUT();
+
+			QRYCHGPRODOUTPUT.UNIBSSBODY body = new QRYCHGPRODOUTPUT.UNIBSSBODY();
+			out.setUNIBSSHEAD(parameters.getUNIBSSHEAD());
+			out.getUNIBSSHEAD().setACTIONCODE("1");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPCODE("8888");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPDESC("程序异常");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPTYPE("1");
+			
+			out.setUNIBSSBODY(body);
+
+			QRYCHGPRODRSP rsp = new QRYCHGPRODRSP();
+
+			QRYCHGPRODREQ req = parameters.getUNIBSSBODY().getQRYCHGPRODREQ();
+
+			rsp.setRESPCODE("8888");
+			rsp.setRESPDESC("程序异常");
+			body.setQRYCHGPRODRSP(rsp);
+			try {
+				LOG.info("响应的报文"+objectMapper.writeValueAsString(out));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return out ;
 		}
 	}
 
@@ -59,13 +98,16 @@ public class SrenewSerUtils {
 	public cn.chinaunicom.ws.srenewser.unibssbody.SRENEWTRADEOUTPUT srenewTrade(
 			cn.chinaunicom.ws.srenewser.unibssbody.SRENEWTRADEINPUT parameters) {
 
-		LOG.info("Executing operation srenewTrade");
-		LOG.info(parameters.toString());
+		
 		try {
 			cn.chinaunicom.ws.srenewser.unibssbody.SRENEWTRADEOUTPUT _return = SpringUtil.getBean(SrenewSerBO.class)
 					.srenewTrade(parameters);
-			
-			LOG.info(_return.toString());
+			try {
+				LOG.info("响应的报文"+objectMapper.writeValueAsString(_return));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			return _return;
 		} catch (Exception ex) {
 
@@ -76,10 +118,19 @@ public class SrenewSerUtils {
 			SRENEWTRADERSP resp = new SRENEWTRADERSP();
 
 			out.setUNIBSSHEAD(parameters.getUNIBSSHEAD());
+			out.getUNIBSSHEAD().setACTIONCODE("1");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPCODE("8888");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPDESC("程序异常");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPTYPE("1");
 			resp.setRESPCODE("8888");
 			resp.setRESPDESC("程序异常");
 			out.getUNIBSSBODY().setSRENEWTRADERSP(resp);
-			LOG.info(out.toString());
+			try {
+				LOG.info("响应的报文"+objectMapper.writeValueAsString(out));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			return out;
 
 		}
@@ -93,14 +144,37 @@ public class SrenewSerUtils {
 	 */
 	public cn.chinaunicom.ws.srenewser.unibssbody.SRENEWSUBOUTPUT srenewSub(
 			cn.chinaunicom.ws.srenewser.unibssbody.SRENEWSUBINPUT parameters) {
-		LOG.info("Executing operation srenewSub");
-		LOG.info(parameters.toString());
+		
 		try {
 			cn.chinaunicom.ws.srenewser.unibssbody.SRENEWSUBOUTPUT _return = SpringUtil.getBean(SrenewSerBO.class).srenewSub(parameters);
+			try {
+				LOG.info("响应的报文"+objectMapper.writeValueAsString(_return));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			return _return;
 		} catch (Exception ex) {
-			ex.printStackTrace();
-			throw new RuntimeException(ex);
+			SRENEWSUBOUTPUT out = new SRENEWSUBOUTPUT();
+			SRENEWSUBOUTPUT.UNIBSSBODY body = new SRENEWSUBOUTPUT.UNIBSSBODY();
+			out.setUNIBSSBODY(body);
+			SRENEWSUBRSP resp = new SRENEWSUBRSP();
+			out.setUNIBSSHEAD(parameters.getUNIBSSHEAD());
+			out.getUNIBSSHEAD().setACTIONCODE("1");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPCODE("8888");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPDESC("程序异常");
+			out.getUNIBSSHEAD().getRESPONSE().setRSPTYPE("1");
+			resp.setRESPCODE("8888");
+			resp.setRESPDESC("程序异常");
+			out.getUNIBSSBODY().setSRENEWSUBRSP(resp);
+			try {
+				LOG.info("响应的报文"+objectMapper.writeValueAsString(out));
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return out;
+			
 		}
 	}
 
