98c55bdc283f4927ec55d8c9280641e6bcb8ce54
root <624191343@qq.com>
2018-05-15 11:34:45 +0800

    spring security jwt 后台微服务管理系统

---
+++ open-layui-web/src/main/java/com/open/capacity/SecurityApplication.java
@@ -0,0 +1,19 @@
+package com.open.capacity;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * 
+ * @author owen 624191343@qq.com
+ *
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+}
