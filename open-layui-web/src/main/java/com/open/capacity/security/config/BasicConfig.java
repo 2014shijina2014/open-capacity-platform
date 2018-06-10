dd8aee126a34bead4a56b75bfb8e400e8bf988b3
owen <624191343@qq.com>
2018-06-10 20:25:05 +0800

    spring security jwt

--- open-layui-web/src/main/java/com/open/capacity/security/config/BasicConfig.java
+++
@@ -1,9 +0,0 @@
-package com.open.capacity.security.config; 
-/** 
-* @author 作者 owen E-mail: 624191343@qq.com
- * @version 创建时间：2018年3月20日 下午10:13:18 类说明
-* 类说明 
-*/
-public class BasicConfig {
-
-}
