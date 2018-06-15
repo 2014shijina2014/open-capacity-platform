4fac43b45cebe93ab2cf4c684365e3ee2b8aa1ef
caoheyang <caoheyang@shinho.net.cn>
2018-06-15 15:42:35 +0800

    测试代码递交 补充注释

--- open-api-gateway/src/main/java/com/open/capacity/client/oauth2/service/impl/RbacServiceImpl.java
+++ open-api-gateway/src/main/java/com/open/capacity/client/oauth2/service/impl/RbacServiceImpl.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.open.capacity.client.oauth2.service.impl;
 
@@ -10,30 +10,34 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.AntPathMatcher;
 
 import com.open.capacity.client.oauth2.service.RbacService;
- 
-/** 
-* @author 作者 owen E-mail: 624191343@qq.com
-* @version 创建时间：2017年12月4日 下午5:32:29 
-* 类说明 
-*   @param request        HttpServletRequest
-*   @param authentication 认证信息
-*   @return 是否有权限
-*/
+
+/**
+ * API 级别权限认证
+ *
+ * @author 作者 owen E-mail: 624191343@qq.com
+ * @version 创建时间：2017年12月4日 下午5:32:29
+ * 类说明
+ */
 
 @Service("rbacService")
 public class RbacServiceImpl implements RbacService {
 
-	private AntPathMatcher antPathMatcher = new AntPathMatcher();
+    private AntPathMatcher antPathMatcher = new AntPathMatcher();
 
-	@Override
-	public boolean hasPermission(HttpServletRequest request, Authentication authentication) {
-		Object principal = authentication.getPrincipal();
+    /**
+     * @param request        HttpServletRequest
+     * @param authentication 认证信息
+     * @return 是否有权限
+     */
+    @Override
+    public boolean hasPermission(HttpServletRequest request, Authentication authentication) {
+        Object principal = authentication.getPrincipal();
 
-		boolean hasPermission = true;
+        //TODO 目前都是true
+        boolean hasPermission = true;
 
-		
-		
-		return hasPermission;
-	}
+
+        return hasPermission;
+    }
 
 }
