92e43fd75da4fd9535dcdd36c1dcefae7f5eac7b
owen <624191343@qq.com>
2018-06-19 20:53:10 +0800

    认证中心

--- open-oauth-center/open-oauth-client/src/main/java/com/open/capacity/UnieapAuthServerApp.java
+++ open-oauth-center/open-oauth-client/src/main/java/com/open/capacity/UnieapAuthServerApp.java
@@ -5,6 +5,7 @@ package com.open.capacity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,9 +18,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 * 类说明 
 */
  
-@SpringBootApplication
+
+
 @Configuration
-@EnableAuthorizationServer
+@EnableDiscoveryClient
+@SpringBootApplication
 public class UnieapAuthServerApp {
 	
 	public static void main(String[] args) {
