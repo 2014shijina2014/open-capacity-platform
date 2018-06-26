383c464183a3318f0be834ba58b2de17c89a0c0f
owen <624191343@qq.com>
2018-06-26 11:55:44 +0800

    精简项目

--- open-cxf-service/src/main/java/cn/chinaunicom/ws/srenewser/unibssbody/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.chinaunicom.cn/SrenewSer/unibssBody", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package cn.chinaunicom.ws.srenewser.unibssbody;
--- open-cxf-service/src/main/java/cn/chinaunicom/ws/srenewser/unibssbody/qrychgprodrsp/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.chinaunicom.cn/SrenewSer/unibssBody/qryChgProdRsp", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package cn.chinaunicom.ws.srenewser.unibssbody.qrychgprodrsp;
