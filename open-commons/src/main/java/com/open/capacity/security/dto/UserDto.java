6a877952a8c58c10418175b6087e01d872468e5e
owen <624191343@qq.com>
2018-06-28 20:36:16 +0800

    公共模块

---
+++ open-commons/src/main/java/com/open/capacity/security/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.open.capacity.security.dto;
+
+import java.util.List;
+
+import com.open.capacity.security.model.SysUser;
+
+public class UserDto extends SysUser {
+
+	private static final long serialVersionUID = -184009306207076712L;
+
+	private List<Long> roleIds;
+
+	public List<Long> getRoleIds() {
+		return roleIds;
+	}
+
+	public void setRoleIds(List<Long> roleIds) {
+		this.roleIds = roleIds;
+	}
+
+}
