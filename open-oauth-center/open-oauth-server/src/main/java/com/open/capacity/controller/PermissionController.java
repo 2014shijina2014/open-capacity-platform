63fd8119639055d32c39849ca7904aea8fbb663e
caoheyang <328690901@qq.com>
2018-07-19 22:32:54 +0800

    多余的引用

--- open-oauth-center/open-oauth-server/src/main/java/com/open/capacity/controller/PermissionController.java
+++ open-oauth-center/open-oauth-server/src/main/java/com/open/capacity/controller/PermissionController.java
@@ -1,13 +1,11 @@
 package com.open.capacity.controller;
 
 import com.open.capacity.fegin.PermissionClient;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.Set;
 
 /**
